ac266d841ccea702ab4991736b0c4c53cef8eb2c
Lokesh Pathrabe <lokesh.pathrabe@searce.com>
2023-08-07 12:41:29 +0530

    subscription for timestamp after 5 seconds

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"example/graph"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// srv.AddTransport(&transport.Websocket{})
+	srv.AddTransport(transport.SSE{}) // <---- This is the important
+
+	// default server
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+	srv.Use(extension.Introspection{}) // introspection is enabled
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
